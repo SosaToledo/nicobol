da3e1b1d948a5e3c62008b58b20cff5fcc43e2fa
Luciano <luchososa1990@hotmail.com.ar>
2017-12-27 01:27:40 -0300

    pagina de contacto
    falta php e iconos.

--- contacto.php
+++ contacto.php
@@ -17,46 +17,68 @@
         }
         .formulario section{
           width: 100%;
-          margin: 20px 0;
+          margin: 60px 0;
         }
         form .texto input{
           width: 100%;
         }
+        #datos{
+          margin-top: 10px;
+        }
+        form button{
+          width: 100%;
+        }
       }
-      
-      *{
-        border: 0.5px dotted grey;
+
+      body{
+        width: 100%;
       }
       .formulario{
         display: flex;
         justify-content: center;
       }
       section{
-        height: 400px;
+        height:300px;
         width: 45%;
-        margin: 0 20px;
+        margin: 60px 20px;
       }
       .texto{
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
+        padding: 10px;
+        border: 3px solid  #B53829;
+        border-bottom: 6px solid  #B53829;
+        font-weight: bold;
       }
       .texto input{
-        width: 50%;
+        color: white;
+        margin-bottom: 15px;
+        background-color:  #B53829;
+        border: 1px solid  #B53829;
       }
-      textarea{
+      .texto textarea{
+        color:  #B53829;
+        background-color: #F4F4F4;
+        width: 100%;
+        margin-top: 20px;
         resize: none;
+        border: 1px solid  #B53829;
       }
       form button{
+        border: 1px solid  #B53829;
+        color: white;
+        background:  #B53829;
         margin-top: 20px;
         float: right;
+        font-weight: bold;
+      }
+      #datos{
+
       }
     </style>
   </head>
   <body>
-    <nav>
-
-    </nav>
+    <?php get_header();
+        get_template_part( 'menu');
+    ?>
     <div class="content formulario">
       <section id="formulario">
         <form class="" action="index.html" method="post">
@@ -69,7 +91,17 @@
         </form>
       </section>
       <section id="datos">
-
+        <div class="">
+          <img src="" alt="">
+          <p>(2477) 432300 - 15595907 - 15586970</p>
+        </div>
+        <div class="">
+          <img src="" alt="">
+          <p>Ameghino 2488 - Pergamino, Bs. As.</p>
+        </div>
+        <div class="">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.8540878269305!2d-60.57158178520459!3d-33.867651080656465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b9cab80ea679d1%3A0x2291517911eed7ef!2sBv.+Ameghino+2488%2C+Pergamino%2C+Buenos+Aires!5e0!3m2!1ses-419!2sar!4v1514347581104" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
       </section>
     </div>
   </body>
--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
     <!-- Font Awesome para iconos y mas -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+
     <!-- Traemos el Boostrap & JQuery -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -20,6 +20,9 @@
 
     <!-- Aca el Animate CSS -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
+
+    <!-- Acá iria tu hermana -->
+    <link rel="stylesheet" href="/css/hermanaDeFrank.css">
 
     <!-- Nuestro CSS -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
