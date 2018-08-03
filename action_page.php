c709125cac547d9993b0d11943e3163c35726078
Luciano <luchososa1990@hotmail.com.ar>
2018-08-03 13:27:41 -0300

    pruebas

--- action_page.php
+++ action_page.php
@@ -1,16 +1,18 @@
 <?php
+/**
+ * Template Name: Pagina de Contactos
+ **/
 /**Toma de variables */
   $persona = $_POST['Name'];
   $correo = $_POST['Email'];
   $consulta = $_POST['Message'];
   
 /**Creación del documento */
-  $destinatario = "contactothinco@gmail.com";
+  $destinatario = "luchososa1990@gmail.com";
   $asunto = "Consulta para Thinco";
   $cuerpo = $persona." envio desde: ".$correo." la consulta: ".$consulta;
 
 
   mail($destinatario,$asunto,$cuerpo);
-  header('location: index.html');
 
 ?>
